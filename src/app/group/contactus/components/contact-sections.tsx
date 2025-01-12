d45cf02759de630edb5b902bf01fdb92ce73b6b7
Yusuf Babatunde <Yusuf.itexpert@gmail.com>
2025-01-12 07:14:37 -0800

    contact us page added

---
+++ src/app/group/contactus/components/contact-sections.tsx
@@ -0,0 +1,62 @@
+import { Box, Container, Typography } from '@mui/material'
+import ContactHero1 from "../../../../assets/img/contactus-hero1.png"
+
+export default function ContactSection() {
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        height: '65vh',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        '&::before': {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          zIndex: 1,
+        },
+        backgroundImage: `url(${ContactHero1.src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Container 
+        maxWidth="sm" 
+        sx={{ 
+          position: 'relative',
+          zIndex: 2,
+          textAlign: 'center',
+        }}
+      >
+        <Typography
+          component="h1"
+          variant="h2"
+          sx={{
+            color: 'white',
+            fontWeight: 400,
+            fontSize:{xs:'22px', md:'26px'},
+            mb: 2,
+          }}
+        >
+          Contact us
+        </Typography>
+        <Typography
+          variant="h6"
+          sx={{
+            color: 'white',
+            fontWeight: 300,
+          }}
+        >
+          We love to hear from you. Please leave us a message.
+        </Typography>
+      </Container>
+    </Box>
+  )
+}
+
